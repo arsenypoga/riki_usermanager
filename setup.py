6a3bfcf48a616badfce67d5489a82bfa86219fb0
Arseny Poga <semiretoja@gmail.com>
2021-04-20 16:07:09 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(
+    name="Riki Usermanager",
+    version="1.0",
+    description="User Manager utilities for the Riki",
+    author="Arseny Poga",
+    author_email="pogaa1@nku.edu",
+    packages=["riki_usermanager"]
+)
